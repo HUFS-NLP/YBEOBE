c88f013da2551545eff0e78e23a62c144d697cac
Eojin <140517360+GeomeunByeol@users.noreply.github.com>
2023-10-08 17:59:25 +0900

    Update LSTM_attention.py

--- run/LSTM_attention.py
+++ run/LSTM_attention.py
@@ -136,7 +136,7 @@ class loss_function_Trainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         labels = inputs["labels"]
         outputs = model(**inputs)
-        logits = outputs[0]
+        logits = outputs.get("logits")
         loss_fct = AsymmetricLoss()
         loss = loss_fct(logits, labels.float())
         return (loss, outputs) if return_outputs else loss

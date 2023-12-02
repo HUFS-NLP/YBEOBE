5c13bf8da407ae0d1fefc13f80e66516d5c7309d
Eojin <140517360+GeomeunByeol@users.noreply.github.com>
2023-12-02 21:44:09 +0900

    Update and rename run/__main__.py to run/train/__main__.py

--- run/train/__main__.py
+++ run/train/__main__.py
@@ -10,13 +10,9 @@ if len(sys.argv) < 2:
 _, command, *arguments = sys.argv
 
 if command == "train":
-    from .train_model_choice import main, parser
-elif command == "inference":
-    from .inference import main, parser
-elif command == "scoring":
-    from .scoring import main, parser
+    from .train import main, parser
 else:
-    print(f'[-] Please type command in ("train", "inference")', file=sys.stderr)
+    print(f'[-] Please type command in ("train")', file=sys.stderr)
     exit(-1)
 
 exit(main(parser.parse_args(arguments)))
